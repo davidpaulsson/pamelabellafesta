ffa1112dfe0047c93affd2950bbec7700fc567ba
David Paulsson <david@davidpaulsson.se>
2020-05-27 12:06:31 +0200

    Revert "Style: animate menu"
    This reverts commit 6b5c179f69045d232355c8081340422931af85c7.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,10 @@
-import { Link, navigate } from 'gatsby';
+import { Link } from 'gatsby';
 import React, { useState, useEffect, useContext } from 'react';
 import styles from './Navbar.module.scss';
 import Collapse from './Collapse';
 import Expand from './Expand';
 import { Ctx } from '../Layout';
 import _ from 'lodash';
-import { motion } from 'framer-motion';
 
 const pages = ['editorial', 'commercial', 'film', 'information'];
 
@@ -23,14 +22,6 @@ const Navbar = ({ location }) => {
   }, [isNotHomePage]);
 
   const { state } = useContext(Ctx);
-
-  const onNavigation = (e, path) => {
-    e.preventDefault();
-    toggleNavIsOpen(false);
-    setTimeout(() => {
-      navigate(path);
-    }, 300);
-  };
 
   return (
     <nav className={styles.nav}>
@@ -66,30 +57,7 @@ const Navbar = ({ location }) => {
         )}
 
         {/** MENU */}
-        <motion.div
-          initial={false}
-          style={{ overflow: 'hidden' }}
-          animate={
-            navIsOpen || !isNotHomePage ? { height: 'auto' } : { height: 0 }
-          }
-        >
-          {pages.map(
-            (page) =>
-              currentPath !== page && (
-                <li key={page} className={styles.navItem}>
-                  <Link
-                    to={'/' + page + '/'}
-                    onClick={(e) => onNavigation(e, '/' + page + '/')}
-                    className={styles.navItemLink}
-                  >
-                    {capitalize(page)}
-                  </Link>
-                </li>
-              ),
-          )}
-        </motion.div>
-
-        {/* {(navIsOpen || !isNotHomePage) &&
+        {(navIsOpen || !isNotHomePage) &&
           pages.map(
             (page) =>
               currentPath !== page && (
@@ -99,7 +67,7 @@ const Navbar = ({ location }) => {
                   </Link>
                 </li>
               ),
-          )} */}
+          )}
       </ul>
     </nav>
   );
