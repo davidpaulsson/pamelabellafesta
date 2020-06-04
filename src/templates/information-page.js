966d07280cd9ed33d76f181a1c3cce424cccf390
David Paulsson <david@davidpaulsson.se>
2020-06-04 10:30:26 +0200

    Refactor: add credits (again)

--- src/templates/information-page.js
+++ src/templates/information-page.js
@@ -1,11 +1,11 @@
 import { graphql } from 'gatsby';
 import React from 'react';
+import useDimensions from 'react-use-dimensions';
+import remark from 'remark';
+import remarkHtml from 'remark-html';
+import recommended from 'remark-preset-lint-recommended';
 import Layout from '../components/Layout';
 import styles from './information-page.module.scss';
-
-import remark from 'remark';
-import recommended from 'remark-preset-lint-recommended';
-import remarkHtml from 'remark-html';
 
 const mdToHtml = (md) =>
   remark().use(recommended).use(remarkHtml).processSync(md).toString();
@@ -17,53 +17,86 @@ export const InformationPageTemplate = ({
   social,
   // representation,
 }) => {
+  const [ref, { height }] = useDimensions();
+
   return (
-    <>
-      <div className={styles.wrapper}>
-        <h3 className={styles.title}>Bio</h3>
-        <div
-          className={[styles.content, styles.bio].join(' ')}
-          dangerouslySetInnerHTML={{ __html: mdToHtml(bio) }}
-        />
-      </div>
-      <div className={styles.wrapper}>
-        <h3 className={styles.title}>Clients</h3>
-        <div
-          className={[styles.content, styles.clients].join(' ')}
-          dangerouslySetInnerHTML={{ __html: mdToHtml(clients) }}
-        />
-      </div>
-      <div className={styles.wrapper}>
-        <h3 className={styles.title}>Contact</h3>
-        <div className={[styles.content, styles.contact].join(' ')}>
-          {contact.map(({ title, text, link }) => (
-            <React.Fragment key={link}>
-              <h4>{title}</h4>
-              <ul>
-                <li>
-                  <a href={link}>{text}</a>
-                </li>
-              </ul>
-            </React.Fragment>
-          ))}
+    <div className={styles.flex}>
+      <div
+        className={styles.flexInner}
+        style={{ minHeight: `calc(100vh - ${height + 24 + 90}px)` }}
+      >
+        <div className={styles.wrapper}>
+          <h3 className={styles.title}>Bio</h3>
+          <div
+            className={[styles.content, styles.bio].join(' ')}
+            dangerouslySetInnerHTML={{ __html: mdToHtml(bio) }}
+          />
+        </div>
+        <div className={styles.wrapper}>
+          <h3 className={styles.title}>Clients</h3>
+          <div
+            className={[styles.content, styles.clients].join(' ')}
+            dangerouslySetInnerHTML={{ __html: mdToHtml(clients) }}
+          />
+        </div>
+        <div className={styles.wrapper}>
+          <h3 className={styles.title}>Contact</h3>
+          <div className={[styles.content, styles.contact].join(' ')}>
+            {contact.map(({ title, text, link }) => (
+              <React.Fragment key={link}>
+                <h4>{title}</h4>
+                <ul>
+                  <li>
+                    <a href={link}>{text}</a>
+                  </li>
+                </ul>
+              </React.Fragment>
+            ))}
+          </div>
+        </div>
+        <div className={styles.wrapper} style={{ borderBottom: 'none' }}>
+          <h3 className={styles.title}>Social</h3>
+          <div className={[styles.content, styles.contact].join(' ')}>
+            {social.map(({ title, text, link }) => (
+              <React.Fragment key={link}>
+                <h4>{title}</h4>
+                <ul>
+                  <li>
+                    <a href={link}>{text}</a>
+                  </li>
+                </ul>
+              </React.Fragment>
+            ))}
+          </div>
         </div>
       </div>
-      <div className={styles.wrapper}>
-        <h3 className={styles.title}>Social</h3>
-        <div className={[styles.content, styles.contact].join(' ')}>
-          {social.map(({ title, text, link }) => (
-            <React.Fragment key={link}>
-              <h4>{title}</h4>
-              <ul>
-                <li>
-                  <a href={link}>{text}</a>
-                </li>
-              </ul>
-            </React.Fragment>
-          ))}
+      <div className={styles.social} ref={ref}>
+        <div
+          className={styles.wrapper}
+          style={{ borderBottom: 'none', marginBottom: 0, paddingBottom: 0 }}
+        >
+          <h3 className={styles.title}>Credits</h3>
+          <div className={[styles.content, styles.clients].join(' ')}>
+            <h4>Design</h4>
+            <ul>
+              <li>
+                <a href="http://www.jornlov.se/">Per Jörnlöv</a>,
+              </li>
+              <li>
+                <a href="https://niklasrosen.se/">Niklas Rosén</a>
+              </li>
+            </ul>
+
+            <h4>Code</h4>
+            <ul>
+              <li>
+                <a href="https://davidpaulsson.se/">David Paulsson</a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
