6f6a705b40b26c64a0c8e3ef36966c960a1346c2
David Paulsson <david@davidpaulsson.se>
2020-10-07 12:58:29 +0200

    Feat: pw protect

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,40 @@
 import './src/styles/global.scss';
+
+import React, { useState } from 'react';
+
+const superSecurePassword = 'kryddhylla';
+
+const LogInCheck = ({ children }) => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [pw, setPw] = useState('');
+
+  const handleSubmit = () => {
+    if (pw === superSecurePassword) {
+      setLoggedIn(true);
+    }
+  };
+
+  if (!loggedIn) {
+    return (
+      <div style={{ marginTop: 16 }}>
+        <form onSubmit={handleSubmit}>
+          <label>
+            <input
+              type="password"
+              placeholder="Password"
+              value={pw}
+              onChange={(e) => setPw(e.target.value)}
+            />
+          </label>
+          <input type="submit" value="Log in" />
+        </form>
+      </div>
+    );
+  }
+
+  return children;
+};
+
+export const wrapRootElement = ({ element }) => {
+  return <LogInCheck>{element}</LogInCheck>;
+};
