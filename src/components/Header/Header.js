bd99d5d8c389ffec90ce7d6fc06d6ef26ca9cbf4
David Paulsson <david@davidpaulsson.se>
2020-10-31 06:20:26 +0100

    Style fixes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,12 +2,14 @@ import { Link } from 'gatsby';
 import React from 'react';
 import Navbar from '../Navbar';
 import styles from './Header.module.scss';
+import Wordmark from './Wordmark';
 
 const Header = ({ location }) => (
   <header className={`${styles.sticky} sticky`}>
     <h1 className={styles.title}>
       <Link to="/" className={styles.link} title="Pamela Bellafesta">
-        Pamela Bellafesta
+        <Wordmark />
+        <span className={styles.screenReaderText}>Pamela Bellafesta</span>
       </Link>
     </h1>
     <Navbar {...{ location }} />
