5d4b5fdfd9f29c9d9419141de3b5bc368f412e04
David Paulsson <david@davidpaulsson.se>
2021-01-08 08:20:42 +0100

    Mobile stules

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -125,9 +125,9 @@ const item = {
 const IndexPage = ({ data, location }) => {
   const { projects } = data;
 
-  useEffect(() => {
-    sessionStorage.setItem('seenLoader', 'true');
-  }, []);
+  // useEffect(() => {
+  //   sessionStorage.setItem('seenLoader', 'true');
+  // }, []);
 
   const fluids = projects.edges.map((edge) => edge.node.featuredImage.node.localFile.childImageSharp.fluid);
 
