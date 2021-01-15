782691b3cd0293c28ccfb676fbb6e2e05b9ed4cb
David Paulsson <david@davidpaulsson.se>
2021-01-15 08:45:12 +0100

    Revert "Remove login"
    This reverts commit 648a59697ae81d9405a8ee1512bf46df07653b37.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,40 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import './src/styles/global.scss';
+
+import React, { useState } from 'react';
+
+const superSecurePassword = 'kryddhylla';
+
+const LogInCheck = ({ children }) => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [pw, setPw] = useState('');
+
+  const handleSubmit = () => {
+    if (pw === superSecurePassword) {
+      setLoggedIn(true);
+    }
+  };
+
+  if (!loggedIn) {
+    return (
+      <div style={{ marginTop: 16 }}>
+        <form onSubmit={handleSubmit}>
+          <label>
+            <input
+              type="password"
+              placeholder="Password"
+              value={pw}
+              onChange={(e) => setPw(e.target.value)}
+            />
+          </label>
+          <input type="submit" value="Log in" />
+        </form>
+      </div>
+    );
+  }
+
+  return children;
+};
+
+// eslint-disable-next-line import/prefer-default-export
+export const wrapRootElement = ({ element }) => <LogInCheck>{element}</LogInCheck>;
